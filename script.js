12a8f8a1e0841ca8b9ff76f5d0ee117aefa8b337
zujaja <zujajatehreem7@gmail.com>
2025-03-28 08:41:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    });
+});
+
 // Contact Form Validation
 const contactForm = document.getElementById('contactForm');
 const formGroups = document.querySelectorAll('.form-group');
